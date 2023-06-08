c150b570be3f089a9900f6f5c35345aa2eaae97f
frederic-favreau <frederic.favreau22@gmail.com>
2023-06-08 11:57:17 +0200

    add model of accomodationType

--- controller/PropertyController.php
+++ controller/PropertyController.php
@@ -7,13 +7,15 @@
         $propertyModel = new PropertyModel();
         $propertyAmenitiesModel = new PropertyAmenitiesModel();
         $houseRulesModel = new HouseRulesModel();
+        $AccomodationTypeModel = new AccomodationTypeModel();
 
         $property = $propertyModel->getOneProperty($id);
         $propertyAmenities = $propertyAmenitiesModel->getPropertyAmenities($id);
         $houseRules = $houseRulesModel -> getHouseRules($id);
+        $AccomodationType = $AccomodationTypeModel -> getAccomodationTypeModel($id);
 
         $oneProperty = $router->generate('baseProperty');
-        echo self::getRender('property.html.twig', ['property' => $property, 'oneProperty' => $oneProperty, 'propertyAmenities' => $propertyAmenities, 'houseRules' => $houseRules]);
+        echo self::getRender('property.html.twig', ['property' => $property, 'oneProperty' => $oneProperty, 'propertyAmenities' => $propertyAmenities, 'houseRules' => $houseRules, 'AccomodationType' => $AccomodationType]);
     }
 }
 
