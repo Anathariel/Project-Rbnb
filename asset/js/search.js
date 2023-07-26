b4f6773f5334fcbfe72d72f62d421084cb1eb6b5
frederic-favreau <frederic.favreau22@gmaom.com>
2023-07-26 21:37:34 +0200

    box search ajax in progress

--- asset/js/search.js
+++ asset/js/search.js
@@ -1,21 +1,43 @@
 $(document).ready(function () {
-  $("#input-search").autocomplete({
-    source: function (request, response) {
-      $.ajax({
-        url: "autocomplete",
-        dataType: "json",
-        data: {
-          term: request.term,
-        },
-        success: function (data) {
-          console.log("Success", data);
-          response(data);
-        },
-        error: function (xhr, status, error) {
-          console.error("Error", status, error);
-        },
-      });
-    },
-    minLength: 2, // Nombre de caractères minimum avant que l'auto-complétion ne commence
+    $("#input-search").autocomplete({
+      source: function (request, response) {
+        $.ajax({
+          url: "autocomplete",
+          dataType: "json",
+          data: {
+            term: request.term,
+          },
+          success: function (data) {
+            response(data);
+          },
+          error: function (xhr, status, error) {
+            console.error("Error", status, error);
+          },
+        });
+      },
+      minLength: 2,
+      open: function (event, ui) {
+        // Ajuster la largeur du menu déroulant pour qu'elle corresponde à celle du texte affiché
+        var menu = $(".ui-autocomplete");
+        var menuItems = menu.find(".ui-menu-item");
+  
+        var maxWidth = 0;
+        menuItems.each(function () {
+          var itemWidth = $(this).outerWidth();
+          maxWidth = Math.max(maxWidth, itemWidth);
+        });
+  
+        menu.outerWidth(maxWidth);
+      },
+      focus: function (event, ui) {
+        // Ajouter la classe pour la surbrillance en pointeur
+        $(".ui-menu-item").removeClass("ui-state-active");
+        $(ui.item[0]).addClass("ui-state-active");
+      },
+      select: function (event, ui) {
+        // Supprimer la classe après la sélection pour éviter la surbrillance persistante
+        $(".ui-menu-item").removeClass("ui-state-active");
+      },
+    });
   });
-});
+  
