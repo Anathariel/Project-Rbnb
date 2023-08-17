2d05bc7eb60dacf6e465e4aeecb8669dd6650760
frederic-favreau <frederic.favreau22@gmaom.com>
2023-08-17 13:20:19 +0200

    search input &travelers, add flapickr for calendar

--- asset/js/reservation.js
+++ asset/js/reservation.js
@@ -1,51 +1,80 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Check Availability
   function checkAvailability() {
-      const arrivalDateElem = document.getElementById("arrivalDate");
-      if (!arrivalDateElem) return;
-      
-      const arrivalDate = arrivalDateElem.value;
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", "/check-availability?date=" + arrivalDate);
-      xhr.onload = function() {
-          if (xhr.status === 200) {
-              const isAvailable = JSON.parse(xhr.responseText);
-              if (!isAvailable) {
-                  alert("La date sélectionnée n'est pas disponible. Veuillez sélectionner une autre date.");
-              }
-          }
-      };
-      xhr.send();
+    const arrivalDateElem = document.getElementById("arrivalDate");
+    if (!arrivalDateElem) return;
+
+    const arrivalDate = arrivalDateElem.value;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "/check-availability?date=" + arrivalDate);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        const isAvailable = JSON.parse(xhr.responseText);
+        if (!isAvailable) {
+          alert(
+            "La date sélectionnée n'est pas disponible. Veuillez sélectionner une autre date."
+          );
+        }
+      }
+    };
+    xhr.send();
+  }
+
+  // Initialiser Flatpickr pour les sélecteurs de date d'arrivée et de départ
+  const arrivalDateInput = document.getElementById("arrivalDate");
+  const departureDateInput = document.getElementById("departureDate");
+
+  if (arrivalDateInput && departureDateInput) {
+    flatpickr(arrivalDateInput, {
+      dateFormat: "Y-m-d", // Ajustez le format selon vos besoins
+      minDate: "today", // Empêche la sélection de dates passées
+      locale: "fr", // Utilisez le format français
+      onChange: function (selectedDates) {
+        updatePrices(); // Mettez à jour les prix lorsque la date d'arrivée change
+      },
+    });
+
+    flatpickr(departureDateInput, {
+      dateFormat: "Y-m-d", // Ajustez le format selon vos besoins
+      minDate: "today", // Empêche la sélection de dates passées
+      locale: "fr", // Utilisez le format français
+      onChange: function (selectedDates) {
+        updatePrices(); // Mettez à jour les prix lorsque la date de départ change
+      },
+    });
   }
 
   // Update Prices
   function updatePrices() {
-      const pricePerNightElem = document.getElementById("pricePerNight");
-      if (!pricePerNightElem) return;
-      
-      const pricePerNight = parseFloat(pricePerNightElem.innerText);
-      const showPriceDetails = document.querySelector(".price-details");
-      if (!showPriceDetails) return;
+    const pricePerNightElem = document.getElementById("pricePerNight");
+    if (!pricePerNightElem) return;
 
-      showPriceDetails.style.display = "none";
-      const arrivalDateElem = document.getElementById("arrivalDate");
-      const departureDateElem = document.getElementById("departureDate");
-      if (!arrivalDateElem || !departureDateElem) return;
+    const pricePerNight = parseFloat(pricePerNightElem.innerText);
+    const showPriceDetails = document.querySelector(".price-details");
+    if (!showPriceDetails) return;
 
-      const arrivalDate = new Date(arrivalDateElem.value);
-      const departureDate = new Date(departureDateElem.value);
-      const numberOfDays = Math.ceil((departureDate - arrivalDate) / (1000 * 60 * 60 * 24));
+    showPriceDetails.style.display = "none";
+    const arrivalDateElem = document.getElementById("arrivalDate");
+    const departureDateElem = document.getElementById("departureDate");
+    if (!arrivalDateElem || !departureDateElem) return;
 
-      if (numberOfDays > 0) {
-          showPriceDetails.style.display = "flex";
-      }
+    const arrivalDate = new Date(arrivalDateElem.value);
+    const departureDate = new Date(departureDateElem.value);
+    const numberOfDays = Math.ceil(
+      (departureDate - arrivalDate) / (1000 * 60 * 60 * 24)
+    );
 
-      const nbDayElem = document.getElementById("nb-day");
-      if (nbDayElem) {
-          nbDayElem.innerText = numberOfDays + " nuit" + (numberOfDays > 1 ? "s" : "");
-      }
+    if (numberOfDays > 0) {
+      showPriceDetails.style.display = "flex";
+    }
 
-      // ... Continue with the rest of your function here
+    const nbDayElem = document.getElementById("nb-day");
+    if (nbDayElem) {
+      nbDayElem.innerText =
+        numberOfDays + " nuit" + (numberOfDays > 1 ? "s" : "");
+    }
+
+    // ... Continue with the rest of your function here
   }
 
   // Event listeners
@@ -54,16 +83,16 @@ document.addEventListener('DOMContentLoaded', function() {
   const travelerSelectElem = document.getElementById("traveler-select");
 
   if (arrivalDateElem) {
-      arrivalDateElem.addEventListener("change", checkAvailability);
-      arrivalDateElem.addEventListener("change", updatePrices);
+    arrivalDateElem.addEventListener("change", checkAvailability);
+    arrivalDateElem.addEventListener("change", updatePrices);
   }
-  
+
   if (departureDateElem) {
-      departureDateElem.addEventListener("change", updatePrices);
+    departureDateElem.addEventListener("change", updatePrices);
   }
-  
+
   if (travelerSelectElem) {
-      travelerSelectElem.addEventListener("change", updatePrices);
+    travelerSelectElem.addEventListener("change", updatePrices);
   }
 
   // Initialize the prices and traveler options on load
--- model/SearchModel.php
+++ model/SearchModel.php
@@ -3,22 +3,74 @@
 class SearchModel extends Model
 {
 
-    public function getSearchResult($searchTerm)
+    // public function getSearchResult($searchTerm)
+    // {
+    //     $searchTerm = '%' . strtolower($searchTerm) . '%';
+
+    //     $req = $this->getDb()->prepare("SELECT `propertyId`, `title`, `description`, `priceNight`, `address`, `city`, `postalCode`, `department`, `region`, `country` FROM `property`"
+    //         . " WHERE LOWER(`title`) LIKE :search_term"
+    //         . " OR LOWER(`priceNight`) LIKE :search_term"
+    //         . " OR LOWER(`address`) LIKE :search_term"
+    //         . " OR LOWER(`city`) LIKE :search_term"
+    //         . " OR LOWER(`postalCode`) LIKE :search_term"
+    //         . " OR LOWER(`department`) LIKE :search_term"
+    //         . " OR LOWER(`region`) LIKE :search_term"
+    //         . " OR LOWER(`country`) LIKE :search_term"
+    //         . " ORDER BY `propertyId`");
+
+    //     $req->bindValue(':search_term', $searchTerm, PDO::PARAM_STR);
+    //     $req->execute();
+
+    //     $result = $req->fetchAll(PDO::FETCH_ASSOC);
+
+    //     return $result;
+    // }
+
+    public function getSearchResult($searchTerm, $arrivalDate, $departureDate, $travelers)
     {
         $searchTerm = '%' . strtolower($searchTerm) . '%';
+        $maxGuestsCondition = "";
 
-        $req = $this->getDb()->prepare("SELECT `propertyId`, `title`, `description`, `priceNight`, `address`, `city`, `postalCode`, `department`, `region`, `country` FROM `property`"
-            . " WHERE LOWER(`title`) LIKE :search_term"
+        if ($travelers !== '') {
+            $maxGuestsCondition = " AND `houserules`.`maxGuests` >= :travelers";
+        }
+
+        $req = $this->getDb()->prepare("SELECT 
+        `property`.`propertyId`, 
+        `title`, 
+        `description`, 
+        `priceNight`, 
+        `address`, 
+        `city`, 
+        `postalCode`, 
+        `department`, 
+        `region`, 
+        `country`, 
+        `houserules`.`houseRulesId`, 
+        `houserules`.`checkInTime`, 
+        `houserules`.`checkOutTime`, 
+        `houserules`.`maxGuests` 
+    FROM `property` 
+    INNER JOIN `houserules` ON `houserules`.`propertyId` = `property`.`propertyId` 
+    WHERE (LOWER(`title`) LIKE :search_term"
             . " OR LOWER(`priceNight`) LIKE :search_term"
             . " OR LOWER(`address`) LIKE :search_term"
             . " OR LOWER(`city`) LIKE :search_term"
             . " OR LOWER(`postalCode`) LIKE :search_term"
             . " OR LOWER(`department`) LIKE :search_term"
             . " OR LOWER(`region`) LIKE :search_term"
-            . " OR LOWER(`country`) LIKE :search_term"
+            . " OR LOWER(`country`) LIKE :search_term)"
+            . " AND (`houserules`.`checkInTime` = :arrival_date OR :arrival_date = '')"
+            . " AND (`houserules`.`checkOutTime` = :departure_date OR :departure_date = '')"
+            . $maxGuestsCondition
             . " ORDER BY `propertyId`");
 
         $req->bindValue(':search_term', $searchTerm, PDO::PARAM_STR);
+        $req->bindValue(':arrival_date', $arrivalDate, PDO::PARAM_STR);
+        $req->bindValue(':departure_date', $departureDate, PDO::PARAM_STR);
+        if ($travelers !== '') {
+            $req->bindValue(':travelers', intval($travelers), PDO::PARAM_INT);
+        }
         $req->execute();
 
         $result = $req->fetchAll(PDO::FETCH_ASSOC);
