47201716d24ef5edf1f999821c846f8264b04e8f
Angéline <anje.jiro@gmail.com>
2023-08-17 11:54:48 +0200

    ✨ Added twig variable for hasnotification dashboard

---
+++ controller/NotificationController.php
@@ -0,0 +1,10 @@
+<?php
+class NotificationController extends Controller
+{
+//Future function to create : hasNotification for the view Dashboard ! So the icon can change depending if we have notifs or not to read.
+/* What should count into notifs : 
+    - new comment on our property
+    - new message (if time for direct message chat)
+    - Reservation/annulations
+*/
+}
