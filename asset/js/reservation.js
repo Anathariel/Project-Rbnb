3020ea4b37c9712be7441e30f8df19c7aeb8d72e
Angéline <anje.jiro@gmail.com>
2023-08-18 11:46:37 +0200

    💄 Calendar and pricing box fix - but bugged price visibility JS to fix

--- asset/js/reservation.js
+++ asset/js/reservation.js
@@ -1,120 +1,79 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Initialiser Flatpickr pour les sélecteurs de date d'arrivée et de départ
-  const arrivalDateInput = document.getElementById("arrivalDate");
-  const departureDateInput = document.getElementById("departureDate");
+  flatpickr("#arrivalDateDisplay", {
+      mode: "range",
+      dateFormat: "Y-m-d",
+      altFormat: "d M Y",
+      minDate: "today",
+      onChange: function(selectedDates, dateStr, instance) {
+          if (selectedDates.length == 2) {
+              // Set the visible inputs
+              document.querySelector("#arrivalDateDisplay").value = flatpickr.formatDate(selectedDates[0], instance.config.altFormat);
+              document.querySelector("#departureDateDisplay").value = flatpickr.formatDate(selectedDates[1], instance.config.altFormat);
+              document.querySelector("#arrivalDate").value = flatpickr.formatDate(selectedDates[0], instance.config.dateFormat);
+              document.querySelector("#departureDate").value = flatpickr.formatDate(selectedDates[1], instance.config.dateFormat);
+              
+              // Call the updatePrices function after setting input values
+              updatePrices();
+          }
+      }
+  });
 
-  if (arrivalDateInput && departureDateInput) {
-    flatpickr(arrivalDateInput, {
-      dateFormat: "Y-m-d", // Ajustez le format selon vos besoins
-      minDate: "today", // Empêche la sélection de dates passées
-      locale: "fr", // Utilisez le format français
-      onChange: function (selectedDates) {
-        updatePrices(); // Mettez à jour les prix lorsque la date d'arrivée change
-      },
-      disable: [
-        function (date) {
-          return !isDateAvailable(date);
-        },
-      ],
-    });
-
-    flatpickr(departureDateInput, {
-      dateFormat: "Y-m-d", // Ajustez le format selon vos besoins
-      minDate: "today", // Empêche la sélection de dates passées
-      locale: "fr", // Utilisez le format français
-      onChange: function (selectedDates) {
-        updatePrices(); // Mettez à jour les prix lorsque la date de départ change
-      },
-      disable: [
-        function (date) {
-          return !isDateAvailable(date);
-        },
-      ],
-    });
-  }
-
-  // Fonction pour vérifier si une date est disponible
+  // Function to check if a date is available
   function isDateAvailable(date) {
-    // Mettez en place votre logique pour vérifier la disponibilité des dates ici
-    // Vous pourriez avoir besoin de récupérer l'ID de la propriété et d'utiliser la fonction reservationModel.isDateAvailableModel
-    // Exemple : return reservationModel.isDateAvailableModel(idPropriete, date.toISOString());
-    return true; // Remplacez par votre logique
+    // Implement your logic to check date availability here
+    return true;
   }
 
-  // Fonction pour calculer et mettre à jour les prix
+  // Function to calculate and update the prices
   function updatePrices() {
     const pricePerNightElem = document.getElementById("pricePerNight");
     const nbDayElem = document.getElementById("nb-day");
     const totalPriceElem = document.getElementById("totalPrice");
     const serviceFeeElem = document.getElementById("serviceFee");
     const taxesElem = document.getElementById("taxes");
-    const totalPriceWithFeesElem =
-      document.getElementById("totalPriceWithFees");
+    const totalPriceWithFeesElem = document.getElementById("totalPriceWithFees");
+    const calculatedTotalElem = document.getElementById("calculatedTotal");
 
-    if (
-      !pricePerNightElem ||
-      !nbDayElem ||
-      !totalPriceElem ||
-      !serviceFeeElem ||
-      !taxesElem ||
-      !totalPriceWithFeesElem
-    ) {
+    if (!pricePerNightElem || !nbDayElem || !totalPriceElem || !serviceFeeElem || !taxesElem || !totalPriceWithFeesElem || !calculatedTotalElem) {
       return;
     }
 
     const pricePerNight = parseFloat(pricePerNightElem.innerText);
     const arrivalDate = new Date(document.getElementById("arrivalDate").value);
-    const departureDate = new Date(
-      document.getElementById("departureDate").value
-    );
-    const numberOfDays = Math.ceil(
-      (departureDate - arrivalDate) / (1000 * 60 * 60 * 24)
-    );
-    const numTravelers = parseInt(
-      document.getElementById("traveler-select").value
-    );
+    const departureDate = new Date(document.getElementById("departureDate").value);
+    const numberOfDays = Math.ceil((departureDate - arrivalDate) / (1000 * 60 * 60 * 24));
+    const numTravelers = parseInt(document.getElementById("traveler-select").value);
 
-    // Calculer les prix et les frais
+    // Calculate prices and fees
     const totalBasePrice = pricePerNight * numberOfDays;
-    const serviceFee = totalBasePrice * 0.05; // Frais de service Loca'r (5%)
-    const taxes = totalBasePrice * 0.02; // Taxes (2%)
+    const serviceFee = totalBasePrice * 0.05; // 5% service fee
+    const taxes = totalBasePrice * 0.02; // 2% taxes
     const totalPriceWithFees = totalBasePrice + serviceFee + taxes;
+    const totalForDays = pricePerNight * numberOfDays;
+    calculatedTotalElem.innerText = totalForDays.toFixed(2) + "€";
 
-    // Mettre à jour les éléments de prix
-    nbDayElem.innerText =
-      numberOfDays + " nuit" + (numberOfDays > 1 ? "s" : "");
+    // Update the price elements
+    nbDayElem.innerText = numberOfDays + " nuit" + (numberOfDays > 1 ? "s" : "");
     serviceFeeElem.innerText = serviceFee.toFixed(2) + "€";
     taxesElem.innerText = taxes.toFixed(2) + "€";
     totalPriceElem.innerText = totalBasePrice.toFixed(2) + "€";
     totalPriceWithFeesElem.innerText = totalPriceWithFees.toFixed(2) + "€";
+    
 
-    // Mise à jour des champs de formulaire cachés pour la soumission
+    // Update hidden form fields for submission
     document.getElementById("startDateInput").value = arrivalDate.toISOString();
     document.getElementById("endDateInput").value = departureDate.toISOString();
     document.getElementById("numTravelersInput").value = numTravelers;
-    document.getElementById("totalPriceInput").value =
-      totalPriceWithFees.toFixed(2);
+    document.getElementById("totalPriceInput").value = totalPriceWithFees.toFixed(2);
   }
 
   // Event listeners
-  const arrivalDateElem = document.getElementById("arrivalDate");
-  const departureDateElem = document.getElementById("departureDate");
   const travelerSelectElem = document.getElementById("traveler-select");
-
-  // Attacher la fonction updatePrices aux modifications du sélecteur de dates
-  if (arrivalDateElem) {
-    arrivalDateElem.addEventListener("change", updatePrices);
-  }
-
-  if (departureDateElem) {
-    departureDateElem.addEventListener("change", updatePrices);
-  }
-
   if (travelerSelectElem) {
-    travelerSelectElem.addEventListener("change", updatePrices);
+      travelerSelectElem.addEventListener("change", updatePrices);
   }
 
-  // Fonction pour mettre à jour l'affichage de la boîte de prix
+  // Function to update the visibility of the price details box
   function updatePriceDetailsVisibility() {
     const arrivalDate = document.getElementById("arrivalDate").value;
     const departureDate = document.getElementById("departureDate").value;
@@ -128,8 +87,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Initialize the prices and traveler options on load
+  // Initialize the prices and traveler options on page load
   updatePrices();
   updatePriceDetailsVisibility();
-  // updateTravelerOptions(); // You can uncomment this if you've defined this function elsewhere
+  // updateTravelerOptions(); // Uncomment this if you've defined this function elsewhere
 });
