c0592b025cd1e96367bd6bdb925c756f25508b44
frederic-favreau <frederic.favreau22@gmaom.com>
2023-08-18 13:08:13 +0200

    searchbar done !

--- model/SearchModel.php
+++ model/SearchModel.php
@@ -2,30 +2,6 @@
 
 class SearchModel extends Model
 {
-
-    // public function getSearchResult($searchTerm)
-    // {
-    //     $searchTerm = '%' . strtolower($searchTerm) . '%';
-
-    //     $req = $this->getDb()->prepare("SELECT `propertyId`, `title`, `description`, `priceNight`, `address`, `city`, `postalCode`, `department`, `region`, `country` FROM `property`"
-    //         . " WHERE LOWER(`title`) LIKE :search_term"
-    //         . " OR LOWER(`priceNight`) LIKE :search_term"
-    //         . " OR LOWER(`address`) LIKE :search_term"
-    //         . " OR LOWER(`city`) LIKE :search_term"
-    //         . " OR LOWER(`postalCode`) LIKE :search_term"
-    //         . " OR LOWER(`department`) LIKE :search_term"
-    //         . " OR LOWER(`region`) LIKE :search_term"
-    //         . " OR LOWER(`country`) LIKE :search_term"
-    //         . " ORDER BY `propertyId`");
-
-    //     $req->bindValue(':search_term', $searchTerm, PDO::PARAM_STR);
-    //     $req->execute();
-
-    //     $result = $req->fetchAll(PDO::FETCH_ASSOC);
-
-    //     return $result;
-    // }
-
     public function getSearchResult($searchTerm, $arrivalDate, $departureDate, $travelers)
     {
         $searchTerm = '%' . strtolower($searchTerm) . '%';
@@ -36,23 +12,26 @@ class SearchModel extends Model
         }
 
         $req = $this->getDb()->prepare("SELECT 
-        `property`.`propertyId`, 
-        `title`, 
-        `description`, 
-        `priceNight`, 
-        `address`, 
-        `city`, 
-        `postalCode`, 
-        `department`, 
-        `region`, 
-        `country`, 
-        `houserules`.`houseRulesId`, 
-        `houserules`.`checkInTime`, 
-        `houserules`.`checkOutTime`, 
-        `houserules`.`maxGuests` 
-    FROM `property` 
-    INNER JOIN `houserules` ON `houserules`.`propertyId` = `property`.`propertyId` 
-    WHERE (LOWER(`title`) LIKE :search_term"
+    `property`.`propertyId`, 
+    `property`.`title`, 
+    `property`.`description`, 
+    `property`.`priceNight`, 
+    `property`.`address`, 
+    `property`.`city`, 
+    `property`.`postalCode`, 
+    `property`.`department`, 
+    `property`.`region`, 
+    `property`.`country`, 
+    `houserules`.`houseRulesId`, 
+    `houserules`.`checkInTime`, 
+    `houserules`.`checkOutTime`, 
+    `houserules`.`maxGuests`
+FROM `property` 
+INNER JOIN `houserules` ON `houserules`.`propertyId` = `property`.`propertyId`
+LEFT JOIN `reservation` ON `reservation`.`propertyId` = `property`.`propertyId`
+AND (:arrival_date BETWEEN `reservation`.`checkInDate` AND `reservation`.`checkoutDate`
+OR :departure_date BETWEEN `reservation`.`checkInDate` AND `reservation`.`checkoutDate`)
+WHERE (LOWER(`title`) LIKE :search_term"
             . " OR LOWER(`priceNight`) LIKE :search_term"
             . " OR LOWER(`address`) LIKE :search_term"
             . " OR LOWER(`city`) LIKE :search_term"
@@ -60,9 +39,8 @@ class SearchModel extends Model
             . " OR LOWER(`department`) LIKE :search_term"
             . " OR LOWER(`region`) LIKE :search_term"
             . " OR LOWER(`country`) LIKE :search_term)"
-            . " AND (`houserules`.`checkInTime` = :arrival_date OR :arrival_date = '')"
-            . " AND (`houserules`.`checkOutTime` = :departure_date OR :departure_date = '')"
             . $maxGuestsCondition
+            . " AND `reservation`.`propertyId` IS NULL"
             . " ORDER BY `propertyId`");
 
         $req->bindValue(':search_term', $searchTerm, PDO::PARAM_STR);
